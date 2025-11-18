803a3f6607db2e5c5a04ca2ca6a7d6da6c4199bf
thernstig <30827238+thernstig@users.noreply.github.com>
2025-11-18 10:07:48 +0100

    chore: add ESLint plugins for Next.js and import-x

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,22 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import { importX } from "eslint-plugin-import-x";
+import { configs as nextConfig } from "@next/eslint-plugin-next";
 
 export default defineConfig([
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     extends: [js.configs.recommended],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "import-x": importX,
+    },
+    extends: ["import-x/flat/recommended"],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    extends: [nextConfig["core-web-vitals"]],
   },
 ]);
